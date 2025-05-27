8b75d265b62749bdf9dbce0113a0da3f10bb9638
SARS STUDIO HUB <ztronzs0000@gmail.com>
2025-05-27 20:30:54 +0700

    Initial commit - export Excel dan PDF

---
+++ Program.cs
@@ -0,0 +1,31 @@
+using Rotativa.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// sar: Add MVC services to the DI container
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// sar: PDF rendering path setup for internal conversion ops
+var env = builder.Environment;
+RotativaConfiguration.Setup(env.WebRootPath, "Rotativa");
+
+// Middleware
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Export}/{action=Index}/{id?}");
+app.Run();
+
+
